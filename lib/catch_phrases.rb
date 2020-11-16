54483f3546d2d3f45394b8b36b52c7a267c2a56c
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-16 00:35:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,10 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
@@ -12,4 +12,4 @@ def link
   puts "It's too dangerous to go alone! Take this."
 end
 
-mario
+toadstool
